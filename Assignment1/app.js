9ace8c55b9776fca50ffb0c9eb7d917063effa8b
tuva9 <tuva9@fpt.edu.vn>
2022-10-04 12:55:05 +0700

    Localstorage and module

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -1,35 +1,47 @@
-// Declaration - Khai báo
-var form = document.querySelector("#form")
-var image = document.querySelector("#image")
+// Localstorage
+// Bộ nhớ của browser
+// Mỗi một application bộ nhớ là 5Mb
+// No expired
+// Có 3 method: getItem, setItem, removeItem
 
-// Validate - Done
-var fields = ["name", "price", "amount", "description"]
-form.onsubmit = function(e) {
-    e.preventDefault()
-    fields.forEach(function(item) {
-        var field = document.querySelector("#" + item)
-        if(field.value === "") {
-            showError(item, "Required")
-        }
-    })
-}
+// Import/Export
+// Tiết kiệm tài nguyên
+// Không gây ra lỗi không mong muốn
+// Có 2 cách để export là {} và default
 
-function showError(id, content) {
-    var element = document.querySelector("#" + id)
-    element.nextElementSibling.innerHTML = content
-}
 
-function clearError(id) {
-    var element = document.querySelector("#" + id)
-    element.nextElementSibling.innerHTML = ""
-}
+// // Declaration - Khai báo
+// var form = document.querySelector("#form")
+// var image = document.querySelector("#image")
 
-// Handle image
-// url: https://image-uploader-anhhtus.herokuapp.com/api/upload
-image.onchange = function(e) {
-    var file = e.target.files[0]
-    var reader = new FileReader();
-    reader.readAsDataURL(file)
-    reader.onloadend = function() {
-    }
-}
+// // Validate - Done
+// var fields = ["name", "price", "amount", "description"]
+// form.onsubmit = function(e) {
+//     e.preventDefault()
+//     fields.forEach(function(item) {
+//         var field = document.querySelector("#" + item)
+//         if(field.value === "") {
+//             showError(item, "Required")
+//         }
+//     })
+// }
+
+// function showError(id, content) {
+//     var element = document.querySelector("#" + id)
+//     element.nextElementSibling.innerHTML = content
+// }
+
+// function clearError(id) {
+//     var element = document.querySelector("#" + id)
+//     element.nextElementSibling.innerHTML = ""
+// }
+
+// // Handle image
+// // url: https://image-uploader-anhhtus.herokuapp.com/api/upload
+// image.onchange = function(e) {
+//     var file = e.target.files[0]
+//     var reader = new FileReader();
+//     reader.readAsDataURL(file)
+//     reader.onloadend = function() {
+//     }
+// }
---
+++ Assignment1/logging.js
@@ -0,0 +1,11 @@
+function log(content) {
+    console.log(content, "Logging service");
+}
+
+// cach 1
+export {
+    log
+}
+
+// cach 2
+export default log
