01b76561b75593ea6dfbced4f2ab4db3cf0851f8
tuva9 <tuva9@fpt.edu.vn>
2022-10-04 13:30:29 +0700

    validation

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -1,47 +1,38 @@
-// Localstorage
-// Bộ nhớ của browser
-// Mỗi một application bộ nhớ là 5Mb
-// No expired
-// Có 3 method: getItem, setItem, removeItem
+// Declaration - Khai báo
+var form = document.querySelector("#form")
+var image = document.querySelector("#image")
 
-// Import/Export
-// Tiết kiệm tài nguyên
-// Không gây ra lỗi không mong muốn
-// Có 2 cách để export là {} và default
+// Validate - Done
+var fields = ["name", "price", "amount", "description"]
+form.onsubmit = function(e) {
+    e.preventDefault()
+    fields.forEach(function(item) {
+        clearError(item)
+    })
+    fields.forEach(function(item) {
+        var field = document.querySelector("#" + item)
+        if(field.value == "") {
+            showError(item, "Trường dữ liệu bắt buộc")
+        }
+    })
+}
 
+function showError(id, content) {
+    var element = document.querySelector("#" + id)
+    element.nextElementSibling.innerHTML = content
+}
 
-// // Declaration - Khai báo
-// var form = document.querySelector("#form")
-// var image = document.querySelector("#image")
+function clearError(id) {
+    var element = document.querySelector("#" + id)
+    element.nextElementSibling.innerHTML = ""
+}
 
-// // Validate - Done
-// var fields = ["name", "price", "amount", "description"]
-// form.onsubmit = function(e) {
-//     e.preventDefault()
-//     fields.forEach(function(item) {
-//         var field = document.querySelector("#" + item)
-//         if(field.value === "") {
-//             showError(item, "Required")
-//         }
-//     })
-// }
-
-// function showError(id, content) {
-//     var element = document.querySelector("#" + id)
-//     element.nextElementSibling.innerHTML = content
-// }
-
-// function clearError(id) {
-//     var element = document.querySelector("#" + id)
-//     element.nextElementSibling.innerHTML = ""
-// }
-
-// // Handle image
-// // url: https://image-uploader-anhhtus.herokuapp.com/api/upload
-// image.onchange = function(e) {
-//     var file = e.target.files[0]
-//     var reader = new FileReader();
-//     reader.readAsDataURL(file)
-//     reader.onloadend = function() {
-//     }
-// }
+// Handle image
+// url: https://image-uploader-anhhtus.herokuapp.com/api/upload
+image.onchange = function(e) {
+    var file = e.target.files[0]
+    var reader = new FileReader();
+    reader.readAsDataURL(file)
+    reader.onloadend = function() {
+    }
+}
