c579b7616b6da5ff893a212192bea1075cec3748
tuva9 <tuva9@fpt.edu.vn>
2022-10-05 17:11:41 +0700

    render menu

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -1,42 +1,75 @@
-// Service
+// Import/ Export
 import {localStorageService} from './service.js'
-import {uploadImage} from './api.js'
 
-// Declaration - Khai báo
-var form = document.querySelector("#form")
-var image = document.querySelector("#image")
+// Localstorage
+// Localstorage getItem, setItem, removeItem
+var menu = [
+    {name: "Bún chả", price: 35000, amount: 10},
+    {name: "Phở bò", price: 40000, amount: 10},
+    {name: "Bún bò", price: 50000, amount: 10},
+]
 
-// Validate - Done
-var elementArray = ["name", "amount", "price", "description"]
-form.onsubmit = function (e) {
-    e.preventDefault()
-    elementArray.forEach(function (item) {
-        clearError(item)
-    })
-    elementArray.forEach(function (item) {
-        var field = document.querySelector("#" + item)
-        if (field.value == "") {
-            showError(item, "Trường dữ liệu bắt buộc")
-        }
-    })
-}
+// Lưu vào localstorage
+localStorageService.set("menu", menu)
 
-function showError(id, content) {
-    var element = document.querySelector("#" + id)
-    element.nextElementSibling.innerHTML = content
-}
+// Lấy dữ liệu từ localstorage ra
+var data = localStorageService.get('menu')
 
-function clearError(id) {
-    var element = document.querySelector("#" + id)
-    element.nextElementSibling.innerHTML = ""
-}
+// Khai báo
+var table = document.querySelector('#table')
 
-// Handle image
-image.onchange = function(e) {
-    var file = e.target.files[0]
-    var reader = new FileReader();
-    reader.readAsDataURL(file)
-    reader.onloadend = function() {
+var content = ""
+data.forEach(function(item, index) {
+    // template string
+    content += `
+    <tr>
+        <td>${index}</td>
+        <td>${item.name}</td>
+        <td>${item.amount}</td>
+        <td>${item.price}</td>
+    </tr>
+    `
+});
 
-    }
-}
+table.innerHTML = content
+
+
+// // Declaration - Khai báo
+// var form = document.querySelector("#form")
+// var image = document.querySelector("#image")
+
+// // Validate - Done
+// var elementArray = ["name", "amount", "price", "description"]
+// form.onsubmit = function (e) {
+//     e.preventDefault()
+//     elementArray.forEach(function (item) {
+//         clearError(item)
+//     })
+//     elementArray.forEach(function (item) {
+//         var field = document.querySelector("#" + item)
+//         if (field.value == "") {
+//             showError(item, "Trường dữ liệu bắt buộc")
+//         }
+//     })
+// }
+
+// function showError(id, content) {
+//     var element = document.querySelector("#" + id)
+//     element.nextElementSibling.innerHTML = content
+// }
+
+// function clearError(id) {
+//     var element = document.querySelector("#" + id)
+//     element.nextElementSibling.innerHTML = ""
+// }
+
+// // Handle image
+// // url: https://image-uploader-anhhtus.herokuapp.com/api/upload
+// image.onchange = function(e) {
+//     var file = e.target.files[0]
+//     var reader = new FileReader();
+//     reader.readAsDataURL(file)
+//     reader.onloadend = function() {
+
+//     }
+// }
