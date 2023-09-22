219693571a2eacad97268f0bbe97187570bf5099
tuva9 <tuva9@fpt.edu.vn>
2023-09-22 13:48:27 +0700

    Buổi 2: IIFE

--- Lesson2/function.js
+++ Lesson2/function.js
@@ -1,37 +1,44 @@
 // 3 cach de tao function trong javascript
-const PI = 3.1412
-// function keyword
-// function perimeter(d) {
+// const PI = 3.1412
+// // function keyword
+// // function perimeter(d) {
+// //     return PI * d
+// // }
+
+// // Anonymous function
+// // const perimeter = function (d) {
+// //     return PI * d
+// // }
+
+// // Array function
+// const perimeter = (d) => {
 //     return PI * d
 // }
 
-// Anonymous function
-// const perimeter = function (d) {
-//     return PI * d
+// // console.log(perimeter(10));
+
+// // Callback
+// const myClass = ["Do Anh Quan", "Dinh Minh Tu", "Nguyen Tien Viet"]
+
+// // const infor = myClass.map(function (sv) {
+// //     return sv + "-" + "WD18203"
+// // })
+
+// // console.log(infor);
+// const wd18203_map = function (arr, callback) {
+//     for (let i = 0; i < arr.length; i++) {
+//         // console.log(arr[i]); 
+//         console.log(callback(arr[i]));
+//     }
 // }
 
-// Array function
-const perimeter = (d) => {
-    return PI * d
-}
-
-// console.log(perimeter(10));
-
-// Callback
-const myClass = ["Do Anh Quan", "Dinh Minh Tu", "Nguyen Tien Viet"]
-
-// const infor = myClass.map(function (sv) {
+// wd18203_map(myClass, function (sv) {
 //     return sv + "-" + "WD18203"
 // })
 
-// console.log(infor);
-const wd18203_map = function (arr, callback) {
-    for (let i = 0; i < arr.length; i++) {
-        // console.log(arr[i]); 
-        console.log(callback(arr[i]));
-    }
-}
 
-wd18203_map(myClass, function (sv) {
-    return sv + "-" + "WD18203"
-})
+// IIFE - Invoke Immediated Function Expression
+// (function() {})()
+(function () {
+    console.log("Xin chao ca lop");
+})()
