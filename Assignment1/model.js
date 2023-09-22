d29f3df7bc32faabcc38b642feb18d97ef0fe029
tuva9 <tuva9@fpt.edu.vn>
2023-09-22 13:17:20 +0700

    Buổi 2: Function

--- Assignment1/model.js
+++
@@ -1,9 +0,0 @@
-var Dishes = function(data) {
-    this.id = 
-    this.name = data.name
-    this.price = data.price
-    this.amount = data.amount
-    this.amount = data.amount
-    this.image = data.image
-    this.description = data.description
-}
--- Lesson6/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.{html,js}", "./index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
