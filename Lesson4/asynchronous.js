6690131cbd35ce8630bdd52c7bafc84f473b2a7f
tuva9 <tuva9@fpt.edu.vn>
2023-10-13 13:39:21 +0700

    Buổi 5: Fetch

--- Lesson4/asynchronous.js
+++ Lesson4/asynchronous.js
@@ -17,17 +17,32 @@ function asyncDoSomeThing(congviec) {
     })
 }
 
-const cv1 = asyncDoSomeThing("Hoàn thành công việc 1")
-const cv2 = asyncDoSomeThing("Hoàn thành công việc 2")
-const cv3 = asyncDoSomeThing("Hoàn thành công việc 3")
+// const cv1 = asyncDoSomeThing("Hoàn thành công việc 1")
+// const cv2 = asyncDoSomeThing("Hoàn thành công việc 2")
+// const cv3 = asyncDoSomeThing("Hoàn thành công việc 3")
 
-cv1.then(function(result) {
-    console.log(result);
-    return cv2
+// cv1.then(function(result) {
+//     console.log(result);
+//     return cv2
+// }).then(function(result) {
+//     console.log(result);
+//     return cv3
+// }).then(function(result) {
+//     console.log(result);
+//     console.log("Hoàn thành công việc 4");
+// })
+
+function showImage(url) {
+    const image = document.createElement('img')
+    image.src = url
+    document.body.appendChild(image)
+}
+
+fetch("https://picsum.photos/200/300")
+.then(function(result) {
+    // console.log(result);
+    showImage(result.url)
+    return fetch("https://picsum.photos/400/600")
 }).then(function(result) {
-    console.log(result);
-    return cv3
-}).then(function(result) {
-    console.log(result);
-    console.log("Hoàn thành công việc 4");
+    showImage(result.url)
 })
